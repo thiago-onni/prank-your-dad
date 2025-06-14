46a957bf6ad7f5fb17c62140bf77daf38cb6e727
tpsantos <tpsantos@mac.lan>
2025-06-14 20:44:10 -0300

    atualizado .env.local

---
+++ checkEnv.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+console.log('API_KEY:', process.env.NEXT_PUBLIC_VAPI_PUBLIC_KEY);
+console.log('API_KEY:', process.env.VAPI_PRIVATE_KEY);
+console.log('API_KEY:', process.env.VAPI_PHONE_NUMBER_ID);
+console.log('API_KEY:', process.env.ELEVENLABS_API_KEY);
+
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    ELEVENLABS_API_KEY: process.env.ELEVENLABS_API_KEY,
+    VAPI_PRIVATE_KEY: process.env.VAPI_PRIVATE_KEY,
+    VAPI_PHONE_NUMBER_ID: process.env.VAPI_PHONE_NUMBER_ID,
+  },
 };
 
 export default nextConfig;
